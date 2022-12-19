82c0e1e2f5809d98f02be93ef1851cb2ea14c879
Shivam Saurav <shivamsaurav8@gmail.com>
2022-12-20 03:30:35 +0530

    First Commit

---
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,30 @@
+package com.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My Program Started!" );
+        // Spring jdbc = JdbcTemplate
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+    	
+    	//insert query
+        
+        String query = "insert into student(id,name,city) values (?,?,?)";
+        
+        // fire the query
+        int result = template.update(query,1,"Saurav", "Ara");
+        System.out.println("number of record inserted.."+ result );
+        
+    }
+}
